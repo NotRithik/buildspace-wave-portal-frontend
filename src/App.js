c72b9f27066b0ff6c3cd9981feae1f97bc3fecbb
NotRithik <rithikum123@gmail.com>
2022-02-21 14:20:29 +0530

    Inital, very bad, test website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from "react-dom";
+
 import './App.css';
+import WavesPanel from './components/WavesPanel'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <WavesPanel addresses={["lmaoded", "ass", "hole"]}/>
+      <div className="wave-div">
+        Click here to wave!<br />
+        <button className="wave-button">Wave! 👋</button>
+      </div>
     </div>
   );
 }
